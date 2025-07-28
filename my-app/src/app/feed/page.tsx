f7fba019429fdc9beb345ba355fb0d69f14a480e
EunSeo Yang <yes24150@gmail.com>
2025-07-28 09:04:28 +0900

    Update page.tsx

--- my-app/src/app/feed/page.tsx
+++ my-app/src/app/feed/page.tsx
@@ -3,10 +3,7 @@ import Feed from "@/app/ui/feed/post"
 import FollowList from "@/app/ui/feed/follow-lists"
 
 export default async function Page() {
-  const res = await fetch("http://localhost:3000/api/feed", {
-    cache: "no-store",
-  })
-
+  const res = await fetch("/api/feed", { cache: "no-store" })
   if (!res.ok) {
     return <div className="p-4 text-red-500">데이터를 불러올 수 없습니다.</div>
   }
